474974d41968c509cdd6b836830ab464c9f23af8
pth1641 <pth16042001@gmail.com>
2022-03-25 12:22:58 +0700

    delete package-lock.json

--- components/Home/StoryList.js
+++ components/Home/StoryList.js
@@ -39,31 +39,31 @@ function StoryList() {
     }, []);
 
     return (
-        <div className="w-full max-h-max bg-gray-200 py-12 px-7">
-            <div className="flex-center flex-col">
+        <div className='w-full max-h-max bg-gray-200 py-12 px-7'>
+            <div className='flex-center flex-col'>
                 <Image
-                    src="/story-logo.png"
-                    alt="Logo"
+                    src='/story-logo.png'
+                    alt='Logo'
                     width={94}
                     height={20}
                 />
-                <h3 className="tracking-widest font-semibold text-xl mt-3">
+                <h3 className='tracking-widest font-semibold text-xl mt-3'>
                     THE STORIES
                 </h3>
             </div>
 
-            <div className="max-w-5xl mx-auto mt-14 mb-10 tracking-wider md:flex-between">
-                <div className="w-full text-xs font-semibold uppercase md:w-48">
+            <div className='max-w-5xl mx-auto mt-14 mb-10 tracking-wider md:flex-between'>
+                <div className='w-full text-xs font-semibold uppercase md:w-48'>
                     <div
-                        className="w-full flex-between pb-1 border-b border-black relative"
+                        className='w-full flex-between pb-1 border-b border-black relative'
                         onClick={toggleDisplay}
                     >
                         SORT BY
-                        <span className="text-lg">
+                        <span className='text-lg'>
                             <BsPlus />
                         </span>
                         {display && (
-                            <ul className="absolute top-6 z-20 px-2 py-1 bg-emerald-200 w-full leading-5 max-h-64 overflow-y-scroll">
+                            <ul className='absolute top-6 z-20 px-2 py-1 bg-emerald-200 w-full leading-5 max-h-64 overflow-y-scroll'>
                                 {categories.map((category, key) => (
                                     <li key={key}>{category}</li>
                                 ))}
@@ -71,16 +71,16 @@ function StoryList() {
                         )}
                     </div>
                 </div>
-                <div className="flex-center flex-col w-full mt-2 gap-y-1 md:flex-row md:max-w-max">
-                    <div className="flex-between text-sm font-medium w-full md:mr-8 md:max-w-max">
+                <div className='flex-center flex-col w-full mt-2 gap-y-1 md:flex-row md:max-w-max'>
+                    <div className='flex-between text-sm font-medium w-full md:mr-8 md:max-w-max'>
                         SEE FILMS
-                        <span className="ml-0.5">
+                        <span className='ml-0.5'>
                             <MdArrowForwardIos />
                         </span>
                     </div>
-                    <div className="flex-between text-sm font-medium w-full md:max-w-max">
+                    <div className='flex-between text-sm font-medium w-full md:max-w-max'>
                         SEE ALL STORIES
-                        <span className="ml-0.5">
+                        <span className='ml-0.5'>
                             <MdArrowForwardIos />
                         </span>
                     </div>
@@ -88,18 +88,18 @@ function StoryList() {
             </div>
 
             {/* Story posts */}
-            <div className="max-w-5xl grid grid-cols-1 mx-auto gap-10 md:grid-cols-2 2xl:grid-cols-4 2xl:max-w-9xl pb-12">
+            <div className='max-w-5xl grid grid-cols-1 mx-auto gap-10 md:grid-cols-2 2xl:grid-cols-4 2xl:max-w-9xl pb-12'>
                 {stories.slice(0, loadMoreLength).map((story, key) => (
                     <StoryItem story={story} key={key} />
                 ))}
             </div>
             {loadMoreLength < maxStoryLength && (
                 <p
-                    className="flex-center font-semibold text-sm tracking-wider cursor-pointer w-max mx-auto"
+                    className='flex-center font-semibold text-sm tracking-wider cursor-pointer w-max mx-auto'
                     onClick={() => setLoadMoreLength(loadMoreLength + 4)}
                 >
                     MORE STORIES
-                    <span className="ml-2">
+                    <span className='ml-2'>
                         <IoIosArrowDown />
                     </span>
                 </p>
--- components/Navbar/Shop.js
+++ components/Navbar/Shop.js
@@ -1,7 +1,61 @@
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import db from '../../firebase';
+import { BsArrowRight } from 'react-icons/bs';
+
 function Shop() {
+    const firestoreData = [];
+    const [shopItem, setShopItem] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            // Fetching data from firestore
+            const querySnapshot = await getDocs(collection(db, 'Shop'));
+            querySnapshot.forEach((doc) => {
+                if (doc.data().square_img !== null) {
+                    firestoreData.push(doc.data());
+                }
+            });
+            setShopItem(firestoreData);
+        }
+        fetchData();
+    }, []);
+    console.log(shopItem);
     return (
         <>
-            <div className="fixed inset-0 bg-white-0.9 z-30"></div>
+            <div className='fixed inset-0 bg-white-0.9 z-30'>
+                <div className='grid grid-cols-4 mt-14'>
+                    {shopItem.map((item) => (
+                        <div className='relative cursor-pointer'>
+                            <img src={item.square_img} className='w-full' />
+                            <div className='absolute inset-0 flex-center flex-col text-lg bg-white-0.9 opacity-0 hover:opacity-100 transition duration-300'>
+                                <h3 className='font-serif'>{item.title}</h3>
+                                <p className='font-semibold text-emerald-500 text-sm'>
+                                    {item.sale ? (
+                                        <>
+                                            <strike className='mr-2'>
+                                                ${item.cost} USD
+                                            </strike>
+                                            <span>${item.sale} USD</span>
+                                        </>
+                                    ) : (
+                                        <span>${item.cost} USD</span>
+                                    )}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <a
+                    type='button'
+                    className='flex-center w-full py-4 bg-gray-100 font-medium cursor-pointer hover:text-white hover:bg-emerald-500 transition duration-300'
+                >
+                    SHOP ALL
+                    <span className='ml-2 font-bold text-xl'>
+                        <BsArrowRight />
+                    </span>
+                </a>
+            </div>
         </>
     );
 }
